f66af69a9fcb13f4bfa725f0e496dd6f7b83da4e
dfsek <dfsek@protonmail.com>
2022-02-28 10:38:28 -0700

    add lazy class

---
+++ src/main/java/org/corpnet/data/util/Lazy.java
@@ -0,0 +1,24 @@
+package org.corpnet.data.util;
+
+import java.util.function.Supplier;
+
+public final class Lazy<T> {
+    private final Supplier<T> value;
+    private T result;
+    private volatile boolean calc = false;
+
+    public static <T> Lazy<T> of(Supplier<T> value) {
+        return new Lazy<>(value);
+    }
+
+    private Lazy(Supplier<T> value) {
+        this.value = value;
+    }
+
+    public T get() {
+        if (calc) return result;
+        result = value.get();
+        calc = true;
+        return result;
+    }
+}
